22f4989e7c220cd806488457645e271844eeb67e
Frank Slyne <frank.slyne@gmail.com>
2016-09-12 14:58:28 +0100

    additional scenarios

---
+++ sim.py
@@ -0,0 +1,25 @@
+from queue import *
+
+scenario =0
+
+
+sched=scheduler(tick=0.001,finish=10)
+
+traf=trafgen('traf',ms1=1)
+term2=terminal('term2')
+
+if scenario == 0:
+  connect('con1',traf.B,term2.A)
+elif scenario == 1:
+  node1=duplex('node1')
+  connect('con1',traf.B,node1.A)
+  connect('con2',term2.A,node1.B)
+elif scenario == 2:
+  stack1 = stack('stack1')
+  stack2 = stack('stack2')
+  connect('linkA',traf.B,stack1.A)
+  connect('linkB',stack1.B,stack2.A)
+  connect('linkC',stack2.B,term2.A)
+
+sched.process()
+
--- tcp.py
+++ tcp.py
@@ -44,7 +44,7 @@ sched=scheduler(tick=0.001,finish=10)
 tcpxmit=tcpgen('tcpxmit',stop=3.0)
 tcprecv=tcpterm('tcprecv')
 
-scenario=6
+scenario=7
 
 # duplex2('node1',ratelimit=1000,MaxSize=100)
 
@@ -84,6 +84,19 @@ elif scenario == 6:
   connect('con3',sw1.B,link.A)
   connect('con3',link.B,sw2.A)
   connect('con4',sw2.B,tcprecv.A)
+elif scenario == 7:
+  sw1=vswitch('sw1',"","Dot1Q()")
+  sw2=vswitch('sw2',"Dot1Q()","MPLS()")
+  sw3=vswitch('sw3',"MPLS()","")
+  link1=datalink('link1',latency=10,trace=True)
+  link2=datalink('link2',latency=50,trace=True)
+  connect('con3',tcpxmit.B,sw1.A)
+  connect('con3',sw1.B,link1.A)
+  connect('con3',link1.B,sw2.A)
+  connect('con3',sw2.B,link2.A)
+  connect('con3',link2.B,sw3.A)
+  connect('con4',sw3.B,tcprecv.A)
+
 
 sched.process()
 
